2f1e6395810e4161e41c353e3f539a770c8a0f58
nachum freedman <nochum09@gmail.com>
2017-09-07 12:38:38 +0300

    component build complete

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="fml-select-container">
+          <div className="fml-first">
+            First
+          </div>
+          <div className="fml-middle">
+            Middle
+          </div>
+          <div className="fml-last">
+            Last
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
